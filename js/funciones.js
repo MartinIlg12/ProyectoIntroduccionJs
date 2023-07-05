bc4e2f98a96fc44e630d3c1f76c29fb285bb1042
MartinIlg12 <MartinIlg12@users.noreply.github.com>
2023-07-05 18:50:37 -0500

    confirmar10

--- js/funciones.js
+++ js/funciones.js
@@ -12,3 +12,11 @@ console.log("El resultado de la suma es: "+Sumar(4,5));
 //Funcion expresada anonimo arrow flech
 const saludo =(nombre) => ("Hola buenas noches "  +nombre);
 console.log(saludo("Pablo"));
+const suma = (a,b)=> (a+b);
+console.log("La suma de los numeros es "+suma(5,6));
+
+const sum =(x,y) =>{
+    let resultado=x+y;
+    return `El resultado de la suma es  ${resultado}`
+}
+console.log(sum(4,5));
